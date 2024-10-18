e86a5e16d6f63bfaab525eb546f23af5880889db
nayanavnair01 <nayananair2006@gmail.com>
2024-10-18 16:22:18 +0530

    Add files via upload

---
+++ inputtask4.py
@@ -0,0 +1,14 @@
+'''
+Author: Nayana V Nair
+Date: 18/10/2024
+Description: program that uses functions from the math module to
+perform the following operations on a number
+'''
+import math
+number=int(input("enter a number:"))
+square_root=math.sqrt(number)
+print("square root of ",number,":",square_root)
+factorial=(math.factorial(number))
+print("factorial is" ,factorial)
+power=(math.pow(number,2))
+print("5 raised to 2",power)
