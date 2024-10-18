cf3659e037e2279657130b26712f89a0005f76ef
nayanavnair01 <nayananair2006@gmail.com>
2024-10-18 15:55:56 +0530

    Add files via upload

---
+++ inputtask3.py
@@ -0,0 +1,13 @@
+'''
+Author: Nayana V Nair
+Date: 18/10/2024
+Description:program that demonstrates the usage of arithmetic,
+ comparison, and logical operators.
+'''
+number1=int(input("enter the firt number"))
+number2=int(input("enter the second number"))
+print("sum=",number1+number2, ",division=" ,number1/number2)
+print("is number1 greater than number2?:",number1>number2)
+print("is number1 equal to number2?:",number1==number2)
+print("logical and:" ,number1>0 and number2>0)
+print("logical or:" ,number1> 0 or number2<0)
