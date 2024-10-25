f574d2a640aae6a4db96e5192e224d7a48ccf120
nayanavnair01 <nayananair2006@gmail.com>
2024-10-25 14:43:40 +0530

    Add files via upload

--- positive.py
+++ positive.py
@@ -1,10 +1,12 @@
 '''
 Author=Nayana V Nair
 Date:25/10/2024
-Description:print if number is positive or negative
+Description:print if number is positive negative or zero
 '''
 number1=int(input("enter a number:"))
 if number1>0:
-    print("number is positive")
+    print(number1,"is positive")
+elif number1<0:
+    print(number1, "is negative")
 else:
-    print("number is negative")
+    print(number1,"is zero")
