c6b393e2bda1a14a39d8e3ff1386c95770ab384f
nayanavnair01 <nayananair2006@gmail.com>
2024-10-25 14:22:04 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,7 @@
+number1=int(input("Enter the first number:"))
+number2= int(input("enter the second number:"))
+if number1>number2:
+    print(number1, "is greater than",number2)
+else:
+    print(number2,"is greater than",number1)
+print("remaining statement")
