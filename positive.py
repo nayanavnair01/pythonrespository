40ea094808aebcbf30c77ebab9d4407f63fcc01d
nayanavnair01 <nayananair2006@gmail.com>
2024-10-25 14:35:46 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,10 @@
+'''
+Author=Nayana V Nair
+Date:25/10/2024
+Description:print if number is positive or negative
+'''
+number1=int(input("enter a number:"))
+if number1>0:
+    print("number is positive")
+else:
+    print("number is negative")
